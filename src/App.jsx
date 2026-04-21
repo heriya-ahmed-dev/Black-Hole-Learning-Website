a031a3263c06856c665c17c0a86e2eb008eac06b
Heracitta <heriyaahmedsiraje@gmail.com>
2026-04-21 22:43:14 +0300

    Learning page is made

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './Header.jsx'
 import Parent from './Components/Carousel/FirstList/Parent.jsx';
-import './App.css'
-import CarouselEffect from './Components/Carousel/CarouselEffect.jsx'
+import './App.css';
+import CarouselEffect from './Components/Carousel/CarouselEffect.jsx';
+import Learn from './Components/Carousel/FirstList/Learning/Learn.jsx';
 const App = () => {
   return (
     <div className=''>
        <Header/>
        <CarouselEffect/>
        <Parent/>
+       <Learn/>
     </div>
   )
 }
